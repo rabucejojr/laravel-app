dce330f178b2e4e879dde710110075518240eee9
rabucejo <rogerhapay@gmail.com>
2023-09-02 22:31:03 +0800

    added fullcalendar basic layout

---
+++ resources/js/Components/Calendar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+
+const Calendar = () => {
+    return (
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+            />
+        </>
+    );
+};
+
+export default Calendar;
