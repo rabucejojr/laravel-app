28d6a61c3e95253b0d3ad580ad13a1ffd29cb261
rabucejo <rogerhapay@gmail.com>
2023-09-02 22:57:05 +0800

    initial calendar display

--- resources/js/Components/Calendar.jsx
+++ resources/js/Components/Calendar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 
 function Calendar() {
+
   return (
       <>
           <FullCalendar
-              plugins={[dayGridPlugin]}
-              initialView="dayGridMonth"
+              plugins={[dayGridPlugin, interactionPlugin]}
           />
       </>
   );
