cc78680e37fc368e14302160ac71fb1bf58ab99a
rabucejo <rogerhapay@gmail.com>
2023-09-04 06:41:43 +0800

    removed added functions; added mantine core/hooks

--- resources/js/Components/Calendar.jsx
+++ resources/js/Components/Calendar.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
-
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 
 function Calendar() {
-
-  return (
-      <>
-          <FullCalendar
-              plugins={[dayGridPlugin, interactionPlugin]}
-          />
-      </>
-  );
+    return (
+        <>
+            <FullCalendar plugins={[dayGridPlugin, interactionPlugin]} />
+        </>
+    );
 }
 
-export default Calendar
+export default Calendar;
