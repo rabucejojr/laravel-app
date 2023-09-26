c60e827b105875239f22e2b58d4e39fda605c936
rabucejo <rogerhapay@gmail.com>
2023-09-26 12:13:59 +0800

    removed unwanted space

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -6,7 +6,6 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
                 {/* LOGIN AND REGISTER */}
-
                 <div className="max-w-7xl mx-auto p-6 lg:p-8">
                     <div className="flex justify-center">
                         <img src="DOST.png" width={"200px"} />
