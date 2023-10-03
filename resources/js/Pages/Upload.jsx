e69614b85ab3e846af6da5832d8274a8c88508bb
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-10-03 14:45:11 +0800

    removed code; added file controller and file model

--- resources/js/Pages/Upload.jsx
+++ resources/js/Pages/Upload.jsx
@@ -9,50 +9,6 @@ export default function Order({ auth }) {
             <div className="py-5">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="overflow-hidden sm:rounded-lg">
-                        {/* <div class="flex justify-center items-center">
-                            <form class="bg-white shadow-md rounded px-8 pt-10 pb-8 mb-4 w-1/2">
-                                <div class="mb-4">
-                                    <label
-                                        class="block text-gray-700 text-sm font-bold mb-2"
-                                        for="username"
-                                    >
-                                        Username
-                                    </label>
-                                    <input
-                                        class="border border-gray-300 p-2 w-full rounded"
-                                        type="text"
-                                        id="username"
-                                        name="username"
-                                        placeholder="Enter your username"
-                                        required
-                                    />
-                                </div>
-                                <div class="mb-4">
-                                    <label
-                                        class="block text-gray-700 text-sm font-bold mb-2"
-                                        for="password"
-                                    >
-                                        Password
-                                    </label>
-                                    <input
-                                        class="border border-gray-300 p-2 w-full rounded"
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        placeholder="Enter your password"
-                                        required
-                                    />
-                                </div>
-                                <div class="mb-4">
-                                    <button
-                                        class="bg-blue-500 text-white px-4 py-2 rounded font-semibold hover:bg-blue-700"
-                                        type="submit"
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </form>
-                        </div> */}
                         <div class="max-w-md mx-auto bg-white p-8 my-10 shadow-lg rounded-lg">
                             <h2 class="text-xl mb-4 font-semibold text-gray-800">
                                 File Upload
