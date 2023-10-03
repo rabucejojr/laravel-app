b5555fecde7d3fba69141f4404b0c314d0db3af5
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-10-03 14:18:48 +0800

    newly configured from another pc

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-
 import Calendar from "@/Components/Calendar";
+import TableComponent from '@/Components/Table';
 
 export default function Dashboard({ auth }) {
     return (
@@ -13,7 +13,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <Calendar/>
+                        <TableComponent/>
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Summary.jsx
+++ resources/js/Pages/Summary.jsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
+
 export default function Summary({ auth }) {
     return (
         <AuthenticatedLayout user={auth.user}>
