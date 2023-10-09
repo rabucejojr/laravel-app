e45df75c22fa4998577cec78ffa73e871361778f
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-10-09 10:50:18 +0800

    added getData api to get mysql data

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    //
+    public function getData()
+    {
+        $data = File::all();
+        return response()->json($data, 200);
+    }
+}
