122a8f116271436e7d3de7e7e47f40d9b722ba6d
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-10-09 08:54:22 +0800

    edit edit

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -9,31 +9,31 @@ use Illuminate\Http\Request;
 class FilesController extends Controller
 {
     //
-    public function index(){
-        return Inertia::render('dashboard',[
-            'files'=>File::all(),
+    public function index()
+    {
+        return Inertia::render('dashboard', [
+            'files' => File::all(),
         ]);
-        
     }
 
     public function store(Request $request)
     {
         // Get Data from Upload.jsx
-        // $filegroup = $request->input('filegroup');
-        // $filename = $request->input('filename');
-        // $description = $request->input('description');
-        // $location = $request->input('location');
+        $filegroup = $request->input('filegroup');
+        $filename = $request->input('filename');
+        $description = $request->input('description');
+        $location = $request->input('location');
 
-        // $files = $request->only([$filegroup, $filename, $description, $location]);
-        // $file = File::create($files);
+        $files = $request->only([$filegroup, $filename, $description, $location]);
+        $file = File::create($files);
         // // Diosplays after successful upload
-
-        File::create($request->validate([
-            'filegroup' => ['required', 'max:50'],
-            'filename'  => ['required', 'max:50'],
-            'description'  => ['required', 'max:100'],
-            'location'  => ['required', 'max:100'],
-        ]));
-        return route('files.index');
+        // $request->merge(['filegroup' => $request->input('filegroup', 'SETUP')]);
+        // File::create($request->validate([
+        //     'filegroup' => ['max:50'],
+        //     'filename'  => ['required', 'max:50'],
+        //     'description'  => ['required', 'max:100'],
+        //     'location'  => ['required', 'max:100'],
+        // ]));
+        // return route('files.index');
     }
 }
