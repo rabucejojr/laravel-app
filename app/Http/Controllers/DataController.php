323592092f30a6b06c2589df611a0efe0570ba0f
rabucejo <rogerhapay@gmail.com>
2023-10-15 12:42:28 +0800

    add store func to put data to api

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -15,6 +15,13 @@ class DataController extends Controller
     }
     public function saveData(Request $req)
     {
-        
+        // Get Data from Upload.jsx
+        // SAVE DATA TO MYSQL
+        $files = new File();
+        $files->filegroup = $req->input('filegroup');
+        $files->filename = $req->input('filename');
+        $files->description = $req->input('description');
+        $files->location = $req->input('location');
+        $files->save();
     }
 }
