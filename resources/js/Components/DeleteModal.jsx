4698f36a502cf13c679d3af1255353d651f1dc88
rabucejo <rogerhapay@gmail.com>
2023-10-15 12:50:41 +0800

    delete unwanted code

--- resources/js/Components/DeleteModal.jsx
+++ resources/js/Components/DeleteModal.jsx
@@ -31,26 +31,6 @@ export default function DeleteModal() {
             <Button style={btnStyle} onClick={handleOpen}>
                 <DeleteIcon/>
             </Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                    >
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor
-                        ligula.
-                    </Typography>
-                </Box>
-            </Modal>
         </div>
     );
 }
