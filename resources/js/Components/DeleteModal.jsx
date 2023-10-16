6aad98d647a140a9528de097b810a985e5e21217
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-10-16 17:22:31 +0800

    debugging

--- resources/js/Components/DeleteModal.jsx
+++ resources/js/Components/DeleteModal.jsx
@@ -3,8 +3,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import DeleteIcon from '@mui/icons-material/Delete';
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import CircularProgress from "@mui/material/CircularProgress";
+import Table from "./Table";
 const style = {
     position: "absolute",
     top: "50%",
@@ -21,22 +22,44 @@ const btnStyle = {
     width: "20px",
 };
 
-export default function DeleteModal({row}) {
+export default function DeleteModal({ row }) {
     const [id, setId] = useState(row.values.id);
-    const handleDelete =()=>{
+    const [loading, setLoading] = useState(false);
+    // const handleDelete =()=>{
+    //     axios
+    //         .delete(`http://127.0.0.1:8000/api/delete/${id}`)
+    //         .then((response) => {
+    //             console.log(response.data);
+    //             setLoading(true);
+    //         })
+    //         .catch((error) => {
+    //             console.error("Error deleting item:", error);
+    //             setLoading(false);
+    //         });
+    // };
+
+    console.log(loading);
+    const handleDelete = () => {
+        setLoading(true);
+        // DELETE
         axios
             .delete(`http://127.0.0.1:8000/api/delete/${id}`)
             .then((response) => {
-                console.log(response.data);
+                // console.log(response.data);
+                setLoading(false);
             })
             .catch((error) => {
                 console.error("Error deleting item:", error);
+                setLoading(false);
             });
+        console.log(loading);
     };
+
     return (
         <div>
             <Button style={btnStyle} onClick={handleDelete}>
-                <DeleteIcon/>
+                <DeleteIcon />
+                {loading && <CircularProgress size={20} />}
             </Button>
         </div>
     );
