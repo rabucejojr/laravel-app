62b5781e54e0bb4eb9edf1323697bf656b93845f
rabucejo <rogerhapay@gmail.com>
2023-10-26 16:54:16 +0800

    modifying

--- resources/js/CustomComponents/SearchBar.jsx
+++ resources/js/CustomComponents/SearchBar.jsx
@@ -1,8 +1,23 @@
-import * as React from "react";
+import React,{useState} from "react";
 import { Box, InputAdornment, OutlinedInput, FormControl } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useEffect } from "react";
 
 export default function SearchBar() {
+    const [data, setData] = useState([]);
+    const [records, setRecords] = useState([]);
+    useEffect(()=>{
+        axios
+            .get("http://127.0.0.1:8000/api/data")
+            .then((res) => {
+                setData(res.data);
+                setRecords(res.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    })
+
     return (
         <Box sx={{ display: "flex", flexWrap: "wrap" }}>
             <div>
