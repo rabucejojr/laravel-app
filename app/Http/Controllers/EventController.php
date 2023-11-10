94ab108242fe79775af4852ca22a5ac4e8e327d6
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-11-10 16:13:01 +0800

    partial api test and controller routing, CRUD

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,37 +7,73 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
-    public function saveEvent(Request $req)
+    // FOR API TESTING
+    public function getEvent(Request $req)
     {
-        $message = 'Saved Successfully!';
-        // Get Data from Upload.jsx
-        // SAVE DATA TO MYSQL
-        $files = new Event();
-        $files->title = $req->input('title');
-        $files->name = $req->input('name');
-        $files->save();
-        return response()->json(['message' => $message], 200);
+        // // Get Data from Upload.jsx
+        // // SAVE DATA TO MYSQL
+        // $files = new Event();
+        // $files->title = $req->input('title');
+        // $files->name = $req->input('name');
+        // $files->save();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Get event successfully!'
+        ], 200);
         // return Inertia::render('SimpleSnackbar',['message'=>$message]);
         // return Redirect::route('summary')->with('message', $message);
     }
+    public function saveEvent(Request $req)
+    {
+        // // Get Data from Upload.jsx
+        // // SAVE DATA TO MYSQL
+        // $files = new Event();
+        // $files->title = $req->input('title');
+        // $files->name = $req->input('name');
+        // $files->save();
+        return response()->json([
+            'status' => 200,
+            'message' => 'File saved successfully!'
+        ], 200);
+        // return Inertia::render('SimpleSnackbar',['message'=>$message]);
+        // return Redirect::route('summary')->with('message', $message);
+    }
+    public function updateEvent($id)
+    {
+        // // dd('here');
+        // $file = Event::where('id', $id)->delete();
+
+        // if ($file) {
+        //     // $file->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'File updated successfully!'
+        ], 200);
+        // } else {
+        //     return response()->json([
+        //         'status' => 404,
+        //         'message' => 'Nothing to delete.'
+        //     ], 404);
+        // }
+    }
     public function deleteEvent($id)
     {
-        // dd('here');
-        $file = Event::where('id', $id)->delete();
+        // // dd('here');
+        // $file = Event::where('id', $id)->delete();
 
-        if ($file) {
-            // $file->delete();
+        // if ($file) {
+        //     // $file->delete();
 
-            return response()->json([
-                'status' => 200,
-                'message' => 'File deleted successfully!'
-            ], 200);
-        } else {
-            return response()->json([
-                'status' => 404,
-                'message' => 'Nothing to delete.'
-            ], 404);
-        }
+        return response()->json([
+            'status' => 200,
+            'message' => 'File deleted successfully!'
+        ], 200);
+        // } else {
+        //     return response()->json([
+        //         'status' => 404,
+        //         'message' => 'Nothing to delete.'
+        //     ], 404);
+        // }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DataController;
+use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//FILES ROUTES
 Route::get('/data', [DataController::class, 'getData']);
 Route::post('/save', [DataController::class, 'saveData']);
 Route::put('/update/{id}', [DataController::class, 'updateData']);
 Route::delete('/delete/{id}', [DataController::class, 'deleteData']);
+// EVENT ROUTES
+Route::get('/events', [EventController::class, 'getEvent']);
+Route::post('/events/save', [EventController::class, 'saveEvent']);
+Route::put('/events/update/{id}', [EventController::class, 'updateEvent']);
+Route::delete('/events/delete/{id}', [EventController::class, 'deleteEvent']);
