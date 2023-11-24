527cfa15c54d282dbc438529c6349bb671487d46
Roger Abucejo Jr <rogerhapay@gmail.com>
2023-11-24 18:29:16 +0800

    modifying

--- resources/js/CustomComponents/Calendar.jsx
+++ resources/js/CustomComponents/Calendar.jsx
@@ -14,6 +14,7 @@ function Calendar() {
 
     const dateClicked = (info) => {
         setDate(info.startStr);
+        console.log(date);
     }
     const handleEventAdd = (info) => {
         setEvents([
